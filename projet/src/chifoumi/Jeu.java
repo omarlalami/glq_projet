db33c078ba32efdfa0fd8cee83d9edf6ce5bdf00
l16024220 <l16024220@C-ID-S14-11.salsa.univ-amu.fr>
2019-03-25 09:49:49 +0100

    init

---
+++ projet/src/chifoumi/Jeu.java
@@ -0,0 +1,21 @@
+package chifoumi;
+
+public class Jeu {
+
+	Joueur monJoueur1;
+	Joueur monJoueur2;
+	
+	public Jeu(String nom_joueur_1, String nom_joueur_2) {
+		
+	}
+	
+	private void jouerLeTour(int n) {
+		
+	}
+	
+	
+	public int [] jouerUnePartie() {
+		 
+		return null;
+	 }
+}
---
+++ projet/src/chifoumi/Joueur.java
@@ -0,0 +1,33 @@
+package chifoumi;
+
+public class Joueur {
+	
+	private String nom;
+	private int score;
+	
+	// TODO
+	public Joueur(String nom) {
+		
+	}
+	
+	// TODO
+	public Coup joue() {
+		
+		return null;
+	}
+	
+	public String nom() {	
+		return nom;
+	}
+	
+	public int score() {
+		return score;
+	}
+	
+	// TODO
+	public String toString() {
+		
+		return null;
+	}
+	
+}
